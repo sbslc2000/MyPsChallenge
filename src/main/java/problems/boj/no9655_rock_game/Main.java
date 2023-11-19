475d9bb3588892d97593679ff7c1d52836e970d7
sbslc2000 <sbslc2000@gmail.com>
2023-11-19 20:52:32 +0900

    1119

---
+++ src/main/java/problems/boj/no9655_rock_game/Main.java
@@ -0,0 +1,21 @@
+package problems.boj.no9655_rock_game;
+
+import java.util.*;
+
+/**
+ * 문제 이름 : 돌 게임
+ * 링크 : https://www.acmicpc.net/problem/9655
+ * 알고리즘 분류 : ?
+ * 작성 일시 : 2023-11-19
+ * 관련 링크 :
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        System.out.println(N == 2 || N % 2 == 1 ? "SK" : "CY");
+
+    }
+}
