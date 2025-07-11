4a1c50f02b76899533c629b670f33de06dd86b27
seobeomseok <sbslc2000@gmail.com>
2025-07-11 18:23:46 +0900

    0711

--- src/main/java/problems/boj/solved/no_13305_gas_station/Main.java
+++ src/main/java/problems/boj/solved/no_13305_gas_station/Main.java
@@ -1,7 +1,6 @@
 package problems.boj.solved.no_13305_gas_station;
 
 import java.io.*;
-import java.util.*;
 
 /**
  * 주유소
@@ -12,8 +11,6 @@ public class Main {
     static int N;
     static long[] dists;
     static long[] prices;
-
-    static long[] caches;
     
 
     public static void main(String[] args) throws Exception {
@@ -28,7 +25,6 @@ public class Main {
         dists = new long[N - 1];
         prices = new long[N];
 
-        caches = new long[N];
 
         splited = br.readLine().split(" ");
         for(int i = 0; i < splited.length; i++) {
@@ -40,19 +36,21 @@ public class Main {
             prices[i] = Integer.parseInt(splited[i]);
         }
 
-        caches[N - 2] = dists[N - 2] * prices[N - 2];
-        long totalDist = dists[N - 2];
+        long currentPrice = prices[0];
+        long total = 0;
+        int p = 0;
 
-        for (int i = N - 3; i >= 0; i--) {
-            totalDist += dists[i];
+        while(p <= N - 2) {
+            total += currentPrice * dists[p];
 
-            long a = totalDist * prices[i];
-            long b = prices[i] * dists[i] + caches[i + 1];
+            if (prices[p + 1] < currentPrice) {
+                currentPrice = prices[p + 1];
+            }
 
-            caches[i] = Math.min(a, b);
+            p++;
         }
 
-        System.out.println(caches[0]);
+        System.out.println(total);
     }
     
 }
